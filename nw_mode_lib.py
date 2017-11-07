190e040c3591759f637be87ff27e13e96f9c1618
chbs8 <christian.bs.8@gmail.com>
2017-11-07 15:50:32 +0100

    PKT class added

--- nw_mode_lib.py
+++ nw_mode_lib.py
@@ -26,7 +26,6 @@ from math import *
 #### Network parameters ####
 RF_CH = [0x50, 0x64]                        # UL & DL channels
 TX_CMPLT = RX_CMPLT = 0                     # Completed files
-TX = NEXT = "00"                            # Current and next TX
 PWR_LVL = NRF24.PA_HIGH                     # Transceiver output (HIGH = -6 dBm + 20 dB)
 BRATE = NRF24.BR_250KBPS                    # 250 kbps bit rate
 TDATA = TACK =  0.1                         # Data and ACK frames timeout (in seconds)
@@ -37,24 +36,25 @@ PIPE_TX = [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]    # TX pipe address
 PIPE_RX = [0xe7, 0xe7, 0xe7, 0xe7, 0xe7]    # RX pipe address
 GPIO_TX = 22                                # TX transceiver's CE to Raspberry GPIO
 GPIO_RX = 24                                # RX transceiver's CE to Raspberry GPIO
-TEAM_A = "00"
-TEAM_B = "01"
-TEAM_C = "10"
-TEAM_D = "11"
+TEAM_A = 0
+TEAM_B = 1
+TEAM_C = 2
+TEAM_D = 3
 MY_TEAM = TEAM_C
+TX = MY_TEAM                                # TX for current time slot
+NEXT = TEAM_D                               # TX for next time slot
 TX_POS = zeros(3)
 RX_POS = zeros(3)
 ACK = zeros(3)
 
 
 
-#### Function definitions ####
+#### Classes and function definitions ####
 
 # COMMS initialization
 # Input: none
 # Output: OK (0) er ErrNum (-1)
 def init_comms():
-
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(GPIO_RX, GPIO.OUT)
     GPIO.output(GPIO_RX,1)
@@ -96,53 +96,92 @@ def init_comms():
     return 0
 
 
-# Start network mode. Set a value to initial random timer.
+# Start network mode. Set a value to initial random timer. Wait until TINIT or CTRL received.
 # Input: none
 # Output: OK (0) er ErrNum (-1)
 def start_network():
-
     random.seed(None, 2)
     TINIT = random.uniform(5,10)
+
+    # Option 1
+    start_time = time.time()
+    while(time.time()<start_time+TINIT):
+        if radio_Rx.available(0):
+            # Packet received, check it
+            packet = PKT()
+            packet = read_pkt()
+
+
     return 0
 
-
-# Given a payload dataset, the packet is arranged to be conformed to standard definitions.
-# Input:
-#       - Type: Control (0) or Data (1)
-#       - RX_ID: Only for data packets, receiver ID
-#       - Payload: Bytes corresponding to data, ACKs for control or file data
-# Output: Packet (of size PLOAD_SIZE), corresponding to transceiver's frame payload field
-def generate_pkt(type, rx_id, payload):
-
-    if type:
-        # Data packet
-        if (NO VALID RX ID):    # HOW TO CHECK IDs!!
-            return -1
-
-        else:
-            if len(payload) > (PLOAD_SIZE-HDR_SIZE):
-                return -1
-            else:
-                frame_payload = list("1"+str(rx_id)+str(TX_POS(rx_id))+payload)     # Review how to concatenate strings and bits!!
-                return frame_payload
-
-    else:
-        # Control packet
-        # Payload = ACK1+ACK2+ACK3; NEXT previously updated
-        header = "0"+MY_TEAM+NEXT+"000"
-        frame_payload = list(header+str(ones[8]*ACK(0))+str(ones[8]*ACK(1))+str(ones[8]*ACK(2)))
-        return frame_payload+frame_payload                                                          # Payload repeated twice (8B < 32B)
 
 
 # Send given packet (frame payload)
 # Input:
-#       - Packet: payload field in frame to be sent
+#       - Packet: payload field in frame to be sent (total size <= 32B)
 # Output: OK (0) er ErrNum (-1)
 def send_pkt(packet):
-
-    if len(packet) > 32:
+    if packet.payloadLength > PLOAD_SIZE-HDR_SIZE:
         return -1
 
     else:
         radio_Tx.write(packet)      # Extra checks can be added (other errors may be possible)
         return 0
+
+
+# Packet class type. It includes:
+#       - Type: control (0) or data (1)
+#       - Header: flags and identifiers before payload (chr)
+#       - Payload: packet data (string)
+#       - Payload Length: int with total length of payload (int in B)
+#       - Frame Data: header+payload (chr list)
+class PKT:
+    def __init__(typ=0,header=chr(0),payload=""):
+        self.typ = typ
+        self.header = header
+        self.payload = payload
+        self.payloadLength = len(payload)
+        self.frameData = list(chr(self.header)+self.payload)
+
+
+    # Given a payload dataset, the packet is arranged to be conformed to standard definitions.
+    # Input:
+    #       - Type: Control (0) or Data (1)
+    #       - RX_ID: Only for data packets, receiver ID
+    #       - Payload: Bytes corresponding to data, ACKs for control or file data
+    # Output: OK (0) er ErrNum (-1). Generated packet (of size PLOAD_SIZE), corresponding to transceiver's frame payload field
+    def generate_pkt(self, typ, payload, rx_id=0):
+        if typ:
+            # Data packet
+            # Header = 1 + RX_ID + TX_POS(RX_ID)
+            if (rx_id == MY_TEAM or rx_id > 3):
+                return -1
+
+            else:
+                if len(payload) > (PLOAD_SIZE-HDR_SIZE):
+                    return -1
+                else:
+                    self.typ = typ
+                    self.header = 128+(rx_id<<5)+TX_POS(rx_id)
+                    self.payload = payload
+                    self.payloadLength = len(payload)
+                    self.frameData = list(chr(self.header)+self.payload)   
+                    return 0
+
+        else:
+            # Control packet
+            # Header = 0 + MY_TEAM_ID + NEXT_ID + 000 (Reserved flags); NEXT previously updated.
+            # Payload = ACK0+ACK1+ACK2 (1B each)
+            self.typ = typ
+            self.header = 0+(MY_TEAM<<5)+(NEXT<<3)
+            self.payload = chr(255*ACK(0))+chr(255*ACK(1))+chr(255*ACK(2))
+            self.payloadLength = 3
+            self.frameData = list(chr(self.header)+self.payload)  
+            return 0
+
+
+    # Read input packet from RF interface
+    # Input: none
+    # Output: Packet (of size PLOAD_SIZE), corresponding to transceiver's frame payload field
+    def read_pkt():
+            
