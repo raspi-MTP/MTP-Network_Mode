d2d2dd023ea7679d13f95da5cbd20a44d4ef5ef0
chbs8 <christian.bs.8@gmail.com>
2017-11-22 18:29:44 +0100

    test main

--- test_PKT.py
+++ test_PKT.py
@@ -2,10 +2,19 @@ from nw_mode_lib import PKT
 
 
 def main():
+    init_comms()
+
     packet = PKT()
     packet.generate_pkt(0)
 
     packet.send_pkt()
+
+    # Wait for ACK
+    start_time = time.time()
+    while(not radio_Rx.available(0) and time.time() < start_time + 10):
+        print("Nothing received :(")
+
+    print(packet.header)
 
 
 
