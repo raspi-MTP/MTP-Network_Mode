ed753398d3cf0db71dfaa9cef217a333b3192d75
chbs8 <christian.bs.8@gmail.com>
2017-11-22 18:45:21 +0100

    Test (6)

--- test_PKT.py
+++ test_PKT.py
@@ -7,17 +7,18 @@ def main():
     packet = PKT()
     packet.generate_pkt(0)
 
-    packet.send_pkt()
+    start_time = time.time()
+    while(time.time() < start_time + 10):
+        packet.send_pkt()
 
     # Wait for ACK
-    start_time = time.time()
-    while(not radio_Rx.available(0) and time.time() < start_time + 10):
-        print("Nothing received :(")
+    # while(not radio_Rx.available(0) and time.time() < start_time + 10):
+    #     print("Nothing received :(")
 
-    if radio_Rx.available(0):
-        print(packet.header)
-    else:
-        print("TIMEOUT!")
+    # if radio_Rx.available(0):
+    #     print(packet.header)
+    # else:
+    #     print("TIMEOUT!")
 
 
 
