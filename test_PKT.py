c6dbda6a71650bf2309231adda19474f896a0f28
chbs8 <christian.bs.8@gmail.com>
2017-11-23 11:09:15 +0100

    Testing...

--- test_PKT.py
+++ test_PKT.py
@@ -5,41 +5,42 @@ try:
         init_comms()
 
         # Active mode
-        # packet = PKT()
-        # packet.generate_pkt(0)
+        packet = PKT()
+        packet.generate_pkt(0)
 
-        # while(True):
-        #     start_time = time.time()
-        #     while(not radio_Rx.available(0) and time.time() < start_time + 5):
-        #         packet.send_pkt()
-        #         #print("Nothing received :(")
+        sleep(2)
+        while(True):
+            start_time = time.time()
+            packet.send_pkt()
+            while(not radio_Rx.available(0) and time.time() < start_time + 2):
+                #print("Nothing received :(")
 
-        #     if radio_Rx.available(0):
-        #         packet.read_pkt()
-        #         print("Received ACK: "+packet.header)
-        #     else:
-        #         print("TIMEOUT")
+            if radio_Rx.available(0):
+                packet.read_pkt()
+                print("Received ACK: "+packet.header)
+            else:
+                print("TIMEOUT")
 
 
         # Passive mode
-        while(True):
-            rx_ctrl, packet = received_ctrl()
+        # while(True):
+        #     rx_ctrl, packet = received_ctrl()
 
-            if(rx_ctrl):
-                # Control received. TX and NEXT updated.
-                #print(packet.header)
-                t_send_ack = random.uniform(0,0.05)
-                time.sleep(t_send_ack)
-                # Send ACK
-                send_ack(packet)
-                WAITING_DATA = True
+        #     if(rx_ctrl):
+        #         # Control received. TX and NEXT updated.
+        #         #print(packet.header)
+        #         t_send_ack = random.uniform(0,0.05)
+        #         time.sleep(t_send_ack)
+        #         # Send ACK
+        #         send_ack(packet)
+        #         WAITING_DATA = True
 
-            else:
-                # Timeout
-                print("Timeout")
-                TX = MY_TEAM
-                NEXT = TEAM_D
-                SEND_CTRL = True
+        #     else:
+        #         # Timeout
+        #         print("Timeout")
+        #         TX = MY_TEAM
+        #         NEXT = TEAM_D
+        #         SEND_CTRL = True
 
 
     if __name__=='__main__':
